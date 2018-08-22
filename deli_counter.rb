4c8fe0acc60709e344eaadedaca0a9e74ccba1ca
Christopher Weber <chrisrrweber@gmail.com>
2018-08-22 01:42:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(katz_deli)
   if katz_deli > 1
-    katz_deli.each_with_index 
+    katz_deli.each_with_index do | name 
   else katz_deli.length == 0
     puts "The line is currently empty."
   end
