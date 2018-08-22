53db6e254658de8358a468bf6639d111788a3141
Christopher Weber <chrisrrweber@gmail.com>
2018-08-22 01:50:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ def line (katz_deli)
   if katz_deli.count > 1
     output = "The line is currently:"
     katz_deli.each_with_index do |name, index|
-      output << " #{index +1}. #{name}"
+      output << "#{index +1}. #{name}"
     end
     puts output
   else
