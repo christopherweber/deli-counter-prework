04155ac791be7c395baca2e8a91b3a94c4d1f61e
Christopher Weber <chrisrrweber@gmail.com>
2018-08-22 01:48:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,23 @@
 def line(katz_deli)
   if katz_deli > 1
-    currently = "The line is currenty: "
-    katz_deli.each_with_index do |name,index|
+    currently = "The line is currenty:"
+    katz_deli.each_with_index do |name, index|
       currently << "#{index+1}. #{name}"
     end
+    puts currently
   else katz_deli.length == 0
     puts "The line is currently empty."
   end
 end 
+
+# def line (katz_deli)
+#   if katz_deli.count > 1
+#     output = "The line is currently:"
+#     katz_deli.each_with_index do |name, index|
+#       output << " #{index +1}. #{name}"
+#     end
+#     puts output
+#   else
+#     puts "The line is currently empty."
+#   end
+# end
