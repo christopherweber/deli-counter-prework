60ae91a1f46270c5538c276504349ee013009ca9
Christopher Weber <chrisrrweber@gmail.com>
2018-08-22 01:42:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(katz_deli)
   if katz_deli > 1
-    katz_deli.each_with_index do | name 
+    katz_deli.each_with_index do |name,index|
   else katz_deli.length == 0
     puts "The line is currently empty."
   end
